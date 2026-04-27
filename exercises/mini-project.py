446a4e1d8dfbb0d0451d429978a5d0f844671709
nicolaleisabetarosca <nicola.elisabeta.rosca@gmail.com>
2026-04-27 22:43:21 +0100

    adding some math logic

--- exercises/mini-project.py
+++ exercises/mini-project.py
@@ -9,3 +9,37 @@ for numbers in list_numbers:
 
 list_numbers.append(16)
 print (list_numbers)
+
+# from collections import Counter
+
+# counter_no = Counter[0]
+
+even_count = 0
+uneven_count = 0 
+
+for x in list_numbers: 
+    if x % 2 == 0: 
+        even_count = even_count + 1      
+    else: 
+        uneven_count = uneven_count + 1
+        
+print("the count of even numbers is", even_count)
+print("the count of uneven numbers is", uneven_count)
+
+
+# find largest number 
+
+smallest_number = list_numbers[0]
+print("smallest number is", smallest_number)
+
+largest_number = 0
+
+
+for x in list_numbers: 
+    if x > smallest_number: 
+        largest_number = x
+    else: 
+        continue;
+
+
+print("the largest number is", largest_number)
